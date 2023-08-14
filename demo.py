c13100fd19ca1827948124992db0cf329735c80f
Ashwin <ashwinshelar10@gmail.com>
2023-08-14 19:56:39 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,9 +1,2 @@
-import requests
-import json
-from datetime import datetime
-
-# askin the user fro api key
-api_key = input("Please Enter Your API: ")
-
-# asking the user for city name
-city_name = input("Please Enter Your City Name: ")
+name = "Ashwin"
+print("hello "+name+"\n")
